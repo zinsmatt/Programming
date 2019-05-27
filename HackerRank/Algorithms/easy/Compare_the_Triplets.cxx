88eaa7b3c0cbb1637c657e9795a5fb37fcebe1ac
Matthieu Zins <zins.matthieu@gmail.com>
2019-05-27 21:27:15 +0200

    Add problem Compare_the_Triplets.cxx

---
+++ HackerRank/Algorithms/easy/Compare_the_Triplets.cxx
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector < int > solve(int a0, int a1, int a2, int b0, int b1, int b2){
+    // Complete this function
+    vector<int> scores;
+    scores.push_back((int)(a0>b0)+(int)(a1>b1)+(int)(a2>b2));
+    scores.push_back((int)(a0<b0)+(int)(a1<b1)+(int)(a2<b2));
+    return scores;
+}
+
+int main() {
+    int a0;
+    int a1;
+    int a2;
+    cin >> a0 >> a1 >> a2;
+    int b0;
+    int b1;
+    int b2;
+    cin >> b0 >> b1 >> b2;
+    vector < int > result = solve(a0, a1, a2, b0, b1, b2);
+    for (ssize_t i = 0; i < result.size(); i++) {
+        cout << result[i] << (i != result.size() - 1 ? " " : "");
+    }
+    cout << endl;
+    
+
+    return 0;
+}
