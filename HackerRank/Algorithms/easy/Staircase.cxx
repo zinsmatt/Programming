df77a87a3d9805575afc6da8b51acda1fe01033e
Matthieu Zins <zins.matthieu@gmail.com>
2019-05-27 21:27:15 +0200

    Add problem Staircase.cxx

---
+++ HackerRank/Algorithms/easy/Staircase.cxx
@@ -0,0 +1,29 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin >> n;
+    for(int i=1;i<=n;i++)
+    {
+        int j=0;
+        while(j<n-i)
+        {
+            cout << " ";
+            j++;
+        }
+        while(j<n)
+        {
+            cout << "#";
+            j++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
