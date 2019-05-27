60274f6054355767543390da3507ca49ee8c0eeb
Matthieu Zins <zins.matthieu@gmail.com>
2019-05-27 21:27:15 +0200

    Add problem Grading_Students.cxx

---
+++ HackerRank/Algorithms/easy/Grading_Students.cxx
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the gradingStudents function below.
+ */
+vector<int> gradingStudents(vector<int> grades) {
+    /*
+     * Write your code here.
+     */
+    auto res = grades;
+    for (auto& g : res)
+    {
+        if (g >= 38)
+        {
+            if (g % 5 == 3) g += 2;
+            else if (g % 5 == 4) g += 1;
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<int> grades(n);
+
+    for (int grades_itr = 0; grades_itr < n; grades_itr++) {
+        int grades_item;
+        cin >> grades_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        grades[grades_itr] = grades_item;
+    }
+
+    vector<int> result = gradingStudents(grades);
+
+    for (int result_itr = 0; result_itr < result.size(); result_itr++) {
+        fout << result[result_itr];
+
+        if (result_itr != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
