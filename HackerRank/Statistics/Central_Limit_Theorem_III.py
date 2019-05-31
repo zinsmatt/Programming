c09588198535548b1ec7d4175cc05e02f7ad33d9
zinsmatt <zins.matthieu@gmail.com>
2019-05-31 23:02:22 +0200

    Add problem Central Limit Theorem III

---
+++ HackerRank/Statistics/Central_Limit_Theorem_III.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import math as m
+
+# sample standard deviation
+std = 80 / m.sqrt(100)
+
+print (500-1.96*std)
+print (500+1.96*std)
