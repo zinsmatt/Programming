bf96d427a5690e360180b2f7ee678e3f79312824
Matthieu Zins <zins.matthieu@gmail.com>
2019-06-12 13:18:12 +0200

    Add problem Between the Offices

---
+++ CodeForces/867A-Between_the_Offices.py
@@ -0,0 +1,10 @@
+n = int(input())
+s = input()
+sf = 0
+fs = 0
+for i in range(n-1):
+    if s[i] == "F" and s[i+1] == "S":
+        fs += 1
+    elif s[i] == "S" and s[i+1] == "F":
+        sf += 1
+print("YES" if sf > fs else "NO")
