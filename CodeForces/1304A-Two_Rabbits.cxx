e46875a69a7b1c8c086dbbede1958264dc008e5a
zinsmatt <zins.matthieu@gmail.com>
2020-02-22 23:49:41 +0100

    Add problem Two Rabbits

---
+++ CodeForces/1304A-Two_Rabbits.cxx
@@ -0,0 +1,82 @@
+// This is just a template solution used as initial script
+
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include <stdlib.h>     /* abs */
+
+ 
+#define F(i, n) for (ll i = 0; i < n; ++i)
+
+#define VPRINT(name, v) \
+    cout << name << " : "; \
+    for (auto& e : v) { \
+        cout << e << " "; \
+    } \
+    cout << std::endl; \
+
+using ll =  long long;
+using ull = unsigned long long;
+ 
+using namespace std;
+ 
+#define INF std::numeric_limits<int>::max()
+
+
+int dfs(const vector<vector<int>>& g, vector<int>& dist, int start, int end=-1)
+{
+	// g is a list of neighbours for each node
+    // dist is assumed to be filled with INF
+    queue<int> q;
+    q.push(start);
+    queue<int> d;
+    d.push(0);
+    while (q.size() > 0)
+    {
+        auto x = q.front();
+        auto dd = d.front();
+        q.pop();
+        d.pop();
+
+        if (dist[x] == INF)
+        {
+            dist[x] = dd;
+            if (end == x)
+                break;
+
+            for (auto e: g[x]) {
+                q.push(e);
+                d.push(dd+1);
+            }
+        }
+    }
+    if (end >= 0 && end < dist.size())
+        return dist[end];
+    else
+        return -1;
+}
+
+
+int main()
+{
+    
+	ll tt;
+	cin >> tt;
+	F(t, tt)
+	{
+		ll x, y, a, b;
+        cin >> x >>y >> a >> b;
+        ll num = y - x;
+        ll den = a + b;
+        if (num % den == 0)
+        {
+            cout << num / den << std::endl;
+        }
+        else
+        {
+            cout << "-1\n";
+        }
+	}
+
+    return 0;
+}
