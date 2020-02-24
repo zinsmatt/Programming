c2ce58371001a9757963eb367c17889d87e3c5f9
zinsmatt <zins.matthieu@gmail.com>
2020-02-24 09:02:08 +0100

    Add problem Mislove has lost an array

---
+++ CodeForces/1204B-Mislove_Has_Lost_an_Array.cxx
@@ -0,0 +1,80 @@
+// This is just a template solution used as initial script
+
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include <stdlib.h>     /* abs */
+
+ 
+#define F(i, n) for (ll i = 0; i < n; ++i)
+
+#define VPRINT(name, v) \
+    cout << name << " : "; \
+    for (auto& e : v) { \
+        cout << e << " "; \
+    } \
+    cout << std::endl; \
+
+using ll =  long long;
+using ull = unsigned long long;
+ 
+using namespace std;
+ 
+#define INF std::numeric_limits<int>::max()
+
+
+int dfs(const vector<vector<int>>& g, vector<int>& dist, int start, int end=-1)
+{
+	// g is a list of neighbours for each node
+    // dist is assumed to be filled with INF
+    queue<int> q;
+    q.push(start);
+    queue<int> d;
+    d.push(0);
+    while (q.size() > 0)
+    {
+        auto x = q.front();
+        auto dd = d.front();
+        q.pop();
+        d.pop();
+
+        if (dist[x] == INF)
+        {
+            dist[x] = dd;
+            if (end == x)
+                break;
+
+            for (auto e: g[x]) {
+                q.push(e);
+                d.push(dd+1);
+            }
+        }
+    }
+    if (end >= 0 && end < dist.size())
+        return dist[end];
+    else
+        return -1;
+}
+
+
+int main()
+{
+    ll n, l, r;
+    cin >> n >> l >> r;
+
+    int mini = 0;
+    for (int i = 0; i < l; ++i)
+    {
+        mini += pow(2, i);
+    }
+    mini += (n-l);
+
+    ll maxi = 0;
+    for (int i = 0; i < r; ++i)
+    {
+        maxi += pow(2, i);
+    }
+    maxi += (n-r) * pow(2, r-1);
+    cout << mini << " " << maxi << "\n";
+    return 0;
+}
