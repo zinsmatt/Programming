7644b6772cc336f8a2f9d73dfd5fbe04415f1fc4
zinsmatt <zins.matthieu@gmail.com>
2020-03-03 22:24:42 +0100

    Add problem Kuroni and Simple Strings

---
+++ CodeForces/1305B-Kuroni_and_Simple_Strings.cxx
@@ -0,0 +1,104 @@
+// This is just a template solution used as initial script
+
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include <stdlib.h>     /* abs */
+
+ 
+#define F(i, n) for (ll i = 0; i < n; ++i)
+
+#define VPRINT(name, v) \
+    cout << name << " : "; \
+    for (auto& e : v) { \
+        cout << e << " "; \
+    } \
+    cout << std::endl; \
+
+using ll =  long long;
+using ull = unsigned long long;
+ 
+using namespace std;
+ 
+#define INF std::numeric_limits<int>::max()
+#define LLINF std::numeric_limits<long long>::max()
+
+
+template <typename T, typename A>
+int arg_max(std::vector<T, A> const& vec) {
+  return static_cast<int>(std::distance(vec.begin(), max_element(vec.begin(), vec.end())));
+}
+
+template <typename T, typename A>
+int arg_min(std::vector<T, A> const& vec) {
+  return static_cast<int>(std::distance(vec.begin(), min_element(vec.begin(), vec.end())));
+}
+
+int dfs(const vector<vector<int>>& g, vector<int>& dist, int start, int end=-1)
+{
+    // g is a list of neighbours for each node
+    // dist is assumed to be filled with INF
+    queue<int> q;
+    q.push(start);
+    queue<int> d;
+    d.push(0);
+    while (q.size() > 0)
+    {
+        auto x = q.front();
+        auto dd = d.front();
+        q.pop();
+        d.pop();
+
+        if (dist[x] == INF)
+        {
+            dist[x] = dd;
+            if (end == x)
+                break;
+
+            for (auto e: g[x]) {
+                q.push(e);
+                d.push(dd+1);
+            }
+        }
+    }
+    if (end >= 0 && end < dist.size())
+        return dist[end];
+    else
+        return -1;
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+    int l = 0, r = s.size()-1;
+    vector<int> ind;
+    while (l < r)
+    {
+        while (l < r && s[l] == ')')
+            ++l;
+        while (r > l && s[r]== '(')
+            --r;
+        if (l < r){
+            ind.push_back(l+1);
+            ind.push_back(r+1);
+            ++l;
+            --r;
+        }
+    }
+
+    if (ind.size() == 0)
+    {
+        cout << "0\n";
+    }
+    else
+    {
+        sort(ind.begin(), ind.end());
+        cout << "1\n";
+        cout << ind.size() << "\n";
+        for (auto i : ind)
+            cout << i << " ";
+        cout << "\n";
+    }
+    return 0;
+}
