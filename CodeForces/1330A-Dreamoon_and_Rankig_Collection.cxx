43396aaaa20e11e9646492f529fddc9463d65bc5
Matthieu Zins <zins.matthieu@gmail.com>
2020-04-06 13:16:44 +0200

    Add problem Dreamoon and Ranking Collection

---
+++ CodeForces/1330A-Dreamoon_and_Rankig_Collection.cxx
@@ -0,0 +1,89 @@
+// This is just a template solution used as initial script
+
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include <stdlib.h>     /* abs */
+
+ 
+#define F(i, n) for (ll i = 0; i < n; ++i)
+
+#define VPRINT(name, v) \
+    cout << name << " : "; \
+    for (auto& e : v) { \
+        cout << e << " "; \
+    } \
+    cout << std::endl;
+
+#define LPRINT(name, l) \
+    cout << "list " << name << " : "; \
+    for (auto it = l.begin(); it != l.end(); ++it) { \
+        cout << *it << " "; \
+    } \
+    cout << std::endl;
+
+using ll =  long long;
+using ull = unsigned long long;
+ 
+using namespace std;
+ 
+#define INF std::numeric_limits<int>::max()
+#define LLINF std::numeric_limits<long long>::max()
+
+
+template <typename T, typename A>
+int arg_max(std::vector<T, A> const& vec) {
+  return static_cast<int>(std::distance(vec.begin(), max_element(vec.begin(), vec.end())));
+}
+
+template <typename T, typename A>
+int arg_min(std::vector<T, A> const& vec) {
+  return static_cast<int>(std::distance(vec.begin(), min_element(vec.begin(), vec.end())));
+}
+
+vector<int> binary2(ll x)
+{
+	vector<int> res;
+	while (x > 0)
+	{
+		res.push_back(x % 2);
+		x >>= 1;
+	}
+	return res;
+}
+
+int main()
+{
+	ll tt;
+	cin >> tt;
+	F(t, tt)
+	{
+		ll n, x;
+		cin >> n >> x;
+		vector<bool> a(200, 0);
+		F(i, n)
+		{
+			int y;
+			cin >> y;
+			a[y] = true;
+		}
+
+		int i = 1;
+		while (1)
+		{
+			if (a[i] == false)
+			{
+				if (x == 0) {
+					--i;
+					break;
+				}
+				--x;
+			}
+			++i;
+		}
+		cout << i << "\n";
+	}
+
+	
+    return 0;
+}
