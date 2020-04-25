18b53808b5a63dbb8e2f9de3425f3cd85fa7687e
Matthieu Zins <zins.matthieu@gmail.com>
2020-04-25 14:08:51 +0200

    Add problem Candies

---
+++ CodeForces/1343A-Candies.cxx
@@ -0,0 +1,81 @@
+// This is just a template solution used as initial script
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include <stdlib.h>     /* abs */
+
+ 
+#define F(i, n) for (ll i = 0; i < n; ++i)
+
+#define VPRINT(name, v) \
+    cout << name << " : "; \
+    for (auto& e : v) { \
+        cout << e << " "; \
+    } \
+    cout << std::endl;
+
+#define LPRINT(name, l) \
+    cout << "list " << name << " : "; \
+    for (auto it = l.begin(); it != l.end(); ++it) { \
+        cout << *it << " "; \
+    } \
+    cout << std::endl;
+
+using ll =  long long;
+using ull = unsigned long long;
+ 
+using namespace std;
+ 
+#define INF std::numeric_limits<int>::max()
+#define LLINF std::numeric_limits<long long>::max()
+
+
+template <typename T, typename A>
+int arg_max(std::vector<T, A> const& vec) {
+  return static_cast<int>(std::distance(vec.begin(), max_element(vec.begin(), vec.end())));
+}
+
+template <typename T, typename A>
+int arg_min(std::vector<T, A> const& vec) {
+  return static_cast<int>(std::distance(vec.begin(), min_element(vec.begin(), vec.end())));
+}
+
+vector<int> binary2(ll x)
+{
+	vector<int> res;
+	while (x > 0)
+	{
+		res.push_back(x % 2);
+		x >>= 1;
+	}
+	return res;
+}
+
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    ll tt=1;
+    cin >> tt;
+    F(tti, tt)
+	{
+		ll n;
+		cin >> n;
+		ll m = 3;
+		ll a = 4;
+		while (1)
+		{
+			if (n % m == 0)
+			{
+				cout << n / m << "\n";
+				break;
+			}
+			m += a;
+			a *= 2;
+		}
+    }
+
+    return 0;
+}
