c53847a87d96fa5370f8644b3ee748de24302672
Matthieu Zins <zins.matthieu@gmail.com>
2021-08-13 13:16:53 +0200

    Add problem Set Matrix Zeroes

---
+++ LeetCode/medium/Set_Matrix_Zeroes.cxx
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        vector<int> rows(matrix.size(), 0);
+        vector<int> cols(matrix[0].size(), 0);
+        for (int i = 0; i < matrix.size(); ++i)
+        {
+            for (int j = 0; j < matrix[0].size(); ++j)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    rows[i] = 1;
+                    cols[j] = 1;
+                }
+            }
+        }
+          for (int i = 0; i < matrix.size(); ++i)
+        {
+            for (int j = 0; j < matrix[0].size(); ++j)
+            {
+                if (rows[i] || cols[j])
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
