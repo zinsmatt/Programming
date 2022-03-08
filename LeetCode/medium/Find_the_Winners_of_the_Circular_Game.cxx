b29b9d23e448b651bd300c7c023432f3e39feef1
Matthieu Zins <zins.matthieu@gmail.com>
2022-03-08 13:26:17 +0100

    Add problem Find the Winners of the Circular Game

---
+++ LeetCode/medium/Find_the_Winners_of_the_Circular_Game.cxx
@@ -0,0 +1,13 @@
+class Solution {
+    
+    int josephus(int n, int k)
+    {
+        if (n == 1) return 0;
+        return (josephus(n-1, k) + k) % n;
+    }
+public:
+    int findTheWinner(int n, int k) {
+        return josephus(n, k)+1;
+        
+    }
+};
