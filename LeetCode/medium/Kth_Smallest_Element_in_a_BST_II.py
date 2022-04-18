25d346697e0daad550235694fafe5ca6d39b639b
zinsmatt <zins.matthieu@gmail.com>
2022-04-18 11:52:31 +0200

    Add problem Kth smallest Element in a BST (bis)

---
+++ LeetCode/medium/Kth_Smallest_Element_in_a_BST_II.py
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int res = -1;
+    int count = 0;
+    
+    void solve(TreeNode* n, int k) {
+        if (n) {
+
+            solve(n->left, k);
+
+            count++;
+            if (count == k) {
+                res = n->val;
+            }
+
+            solve(n->right, k);
+        }
+    }
+    
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        solve(root, k);
+        return res;
+        
+    }
+};
