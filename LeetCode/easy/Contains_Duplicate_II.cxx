aa51e20849c8f9d1d03ca06ad70bb4535e471eb6
Matthieu Zins <zins.matthieu@gmail.com>
2022-06-28 09:19:58 +0200

    Add problem Contain Duplicate (bis)

--- LeetCode/easy/Contains_Duplicate_II.cxx
+++ LeetCode/easy/Contains_Duplicate_II.cxx
@@ -1,24 +1,13 @@
 class Solution {
 public:
-    bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int, int> m;
-        ++k;
-        for (int i = 0; i < k && i < nums.size(); ++i)
-        {
-            if (m.find(nums[i]) == m.end())
-                m[nums[i]] = 1;
-            else
-                return true;           
-        }
-        
-        for (int i = k; i < nums.size(); ++i)
-        {
-            m[nums[i-k]] = 0;
-            if (m.find(nums[i]) != m.end() && m[nums[i]] == 1)
+    bool containsDuplicate(vector<int>& nums) {
+        std::unordered_set<int> s;
+        for (auto x : nums) {
+            if (s.count(x))
                 return true;
             else
-                m[nums[i]] = 1;
+                s.insert(x);
         }
-        return false;
+        return false; 
     }
 };
