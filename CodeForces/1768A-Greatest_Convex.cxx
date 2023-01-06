e1333c5cfd9b5914d1ac5b2e751c4748c74eec44
zinsmatt <zins.matthieu@gmail.com>
2023-01-06 16:26:11 +0100

    Add problem 1768A Greatest Convex

---
+++ CodeForces/1768A-Greatest_Convex.cxx
@@ -0,0 +1,93 @@
+// This is just a template solution used as initial script
+
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include <stdlib.h>     /* abs */
+
+ 
+#define F(i, n) for (ll i = 0; i < n; ++i)
+
+#define VPRINT(name, v) \
+    cout << name << " : "; \
+    for (auto& e : v) { \
+        cout << e << " "; \
+    } \
+    cout << std::endl; \
+
+using ll =  long long;
+using ull = unsigned long long;
+ 
+using namespace std;
+ 
+#define INF std::numeric_limits<int>::max()
+#define LLINF std::numeric_limits<long long>::max()
+
+
+template <typename T, typename A>
+int arg_max(std::vector<T, A> const& vec) {
+  return static_cast<int>(std::distance(vec.begin(), max_element(vec.begin(), vec.end())));
+}
+
+template <typename T, typename A>
+int arg_min(std::vector<T, A> const& vec) {
+  return static_cast<int>(std::distance(vec.begin(), min_element(vec.begin(), vec.end())));
+}
+
+int dfs(const vector<vector<int>>& g, vector<int>& dist, int start, int end=-1)
+{
+	// g is a list of neighbours for each node
+    // dist is assumed to be filled with INF
+    queue<int> q;
+    q.push(start);
+    queue<int> d;
+    d.push(0);
+    while (q.size() > 0)
+    {
+        auto x = q.front();
+        auto dd = d.front();
+        q.pop();
+        d.pop();
+
+        if (dist[x] == INF)
+        {
+            dist[x] = dd;
+            if (end == x)
+                break;
+
+            for (auto e: g[x]) {
+                q.push(e);
+                d.push(dd+1);
+            }
+        }
+    }
+    if (end >= 0 && end < dist.size())
+        return dist[end];
+    else
+        return -1;
+}
+
+ull fact(ull x) {
+    if (x == 0)
+    return 1;
+    return x * fact(x-1);
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    F (i, t) {
+    // for (int i = 2; i < 30; ++i) {
+    ull x;
+    cin >> x;
+    // x = i;
+    // ull a = fact(x-1);
+    // ull b = fact(x-2);
+    // std::cout << a << " " << b << "\n";
+    // std::cout << a+b << " " << (a+b)%x << "\n";
+    // }
+    cout << x-1 << "\n";
+    }
+    return 0;
+}
