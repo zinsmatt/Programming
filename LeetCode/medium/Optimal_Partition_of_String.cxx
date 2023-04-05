c2eae91fbf1b9fed00b1b2650beac84ba849244d
zinsmatt <zins.matthieu@gmail.com>
2023-04-05 12:31:22 +0200

    Add problem Optimal Partition of String

---
+++ LeetCode/medium/Optimal_Partition_of_String.cxx
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int partitionString(string s) {
+        vector<bool> count(26, false);
+        int res = 0;
+        for (auto c : s) {
+            if (count[c-'a']) {
+                ++res;
+                fill(count.begin(), count.end(), false);
+            }
+            count[c-'a'] = true;
+        }
+
+        return res+1;
+    }
+};
